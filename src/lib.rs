858a367a754e3759eac596e861bbd572c4bbd905
w01fb0ss <44709004+w01fb0ss@users.noreply.github.com>
2022-07-17 17:57:12 +0800

    aaa

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,7 @@ fn flatten_object(
     for (k, v) in nested_dict.iter() {
         let new_k = match parent_key {
             Some(ref key) => format!("{}{}{}", key, sep, k),
-            None => k.clone(),
+            None => format!("{}_", k.clone()),
         };
         // if nested value is object recurse with parent_key
         if let Some(obj) = v.as_object() {
